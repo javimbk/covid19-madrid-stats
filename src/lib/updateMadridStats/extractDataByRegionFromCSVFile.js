825c46999e43bc44e1405c2982e3f33650193e3c
javimbk <javimbk@gmail.com>
2020-04-24 17:14:09 +0200

    feat(eslint): add eslint-config-airbnb

--- src/lib/updateMadridStats/extractDataByRegionFromCSVFile.js
+++ src/lib/updateMadridStats/extractDataByRegionFromCSVFile.js
@@ -3,29 +3,29 @@ import Papa from 'papaparse';
 import fs from 'fs';
 
 export default async function extractDataByRegionFromCSVFile() {
-  const fullData = await loadAndParseFile();
+  const fullData = await loadAndParseFile('./data/raw/latestStats.csv');
   const groupedData = groupDataByRegion(fullData);
 
   return groupedData;
 }
 
 async function loadAndParseFile(filePath) {
-  const csvFile = fs.readFileSync('./data/raw/latestStats.csv');
+  const csvFile = fs.readFileSync(filePath);
   const csvData = csvFile.toString();
 
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     Papa.parse(csvData, {
       header: true,
       complete: (results) => {
         resolve(results.data);
-      }
+      },
     });
   });
 }
 
 function groupDataByRegion(fullData) {
   return fullData.reduce((acc, current) => {
-    const regionId = current['CCAA'];
+    const regionId = current.CCAA;
     if (regionId.length !== 2) { return acc; }
 
     if (!acc[regionId]) {
--- src/lib/updateMadridStats/index.js
+++ src/lib/updateMadridStats/index.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
-import extractDataFromCSVFile from "./extractDataByRegionFromCSVFile";
-import fetchLatestStats from "./fetchLatestStats";
+import extractDataFromCSVFile from './extractDataByRegionFromCSVFile';
+import fetchLatestStats from './fetchLatestStats';
 
 export default async function updateMadridStats() {
   await fetchLatestStats();
   const dataByRegion = await extractDataFromCSVFile();
-  const madridStatsJSON = JSON.stringify(dataByRegion['MD']);
+  const madridStatsJSON = JSON.stringify(dataByRegion.MD);
 
   fs.writeFile('./data/madrid_stats.json', madridStatsJSON, (err) => {
     if (err) {
       console.log('Error writing JSON: ', err);
     } else {
-      console.log('Success!')
+      console.log('Success!');
     }
   });
 }

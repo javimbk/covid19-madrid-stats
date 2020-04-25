d4a9e7286ae28d94221368d84c5c8dec1f46c946
javimbk <javimbk@gmail.com>
2020-04-25 17:13:29 +0200

    Updates

---
+++ precache-manifest.0a37b9707fb0bb25ca3665fe96b4a4cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bbe86f75e0b4b973571a5505f7c57d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee1b71fff5f85ae78fb",
+    "url": "/static/css/main.819fc45b.chunk.css"
+  },
+  {
+    "revision": "64f5b12e3cca1c1d70ba",
+    "url": "/static/js/2.90d2db63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90d2db63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee1b71fff5f85ae78fb",
+    "url": "/static/js/main.666f8186.chunk.js"
+  },
+  {
+    "revision": "51ff0d31741babd508a6",
+    "url": "/static/js/runtime-main.eda49eed.js"
+  }
+]);
